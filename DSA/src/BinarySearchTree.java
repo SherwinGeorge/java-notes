d6f0920ed5e72c5eeded2051c656c38268292030
Sherwin George <89149622+SherwinGeorge@users.noreply.github.com>
2023-03-08 20:43:02 +0530

    Add files via upload

---
+++ DSA/src/BinarySearchTree.java
@@ -0,0 +1,13 @@
+public class BinarySearchTree {
+    private Node root;
+
+    public Node{
+        
+    }
+
+    public BinarySearchTree(int value){
+        Node newNode = new Node(value);
+        root = newNode;
+    }
+
+}
---
+++ DSA/src/Main.java
@@ -0,0 +1,177 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        LinkedList myLL = new LinkedList(1);
+//        myLL.append(69);
+//        myLL.prepend(88);
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+//
+//
+//        System.out.println();
+//        System.out.println(myLL.get(2));
+//
+//        //set
+//        System.out.println();
+//        myLL.set(1,66);
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+//
+//
+//        //insert
+//        System.out.println();
+//        myLL.insert(1,90);
+//        myLL.printList();
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+//
+//        //remove
+//        System.out.println();
+//        myLL.remove(2);
+//        myLL.printList();
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+//
+//        //reverse
+//        System.out.println();
+//        myLL.reverse();
+//        myLL.printList();
+//        myLL.getHead();
+//        myLL.getTail();
+//        myLL.getLength();
+
+//        DoublyLinkedList myDLL = new DoublyLinkedList(8);
+//
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//        myDLL.printList();
+//
+//        System.out.println();
+//
+//        //append
+//        myDLL.append(69);
+//        myDLL.append(45);
+//        myDLL.append(88);
+//        myDLL.append(32);
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //removeLast
+//        myDLL.removeLast();
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //prepend
+//        myDLL.prepend(100);
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //removeFirst
+//        myDLL.removeFirst();
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //get
+//        System.out.println(myDLL.get(3).value);
+//        System.out.println();
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //set
+//        myDLL.set(2,90);
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //insert
+//        myDLL.insert(2,78);
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+//
+//        System.out.println();
+//
+//        //remove
+//        myDLL.remove(2);
+//        myDLL.printList();
+//        myDLL.getHead();
+//        myDLL.getTail();
+//        myDLL.getLength();
+
+//        Stack myStack = new Stack(5);
+//
+//        myStack.getTop();
+//        myStack.getHeight();
+//        myStack.printStack();
+//
+//        System.out.println();
+//        //push
+//        myStack.push(45);
+//        myStack.push(89);
+//        myStack.push(55);
+//
+//        myStack.getTop();
+//        myStack.getHeight();
+//        myStack.printStack();
+//
+//        System.out.println();
+//        //pop
+//        myStack.pop();
+//        myStack.getTop();
+//        myStack.getHeight();
+//        myStack.printStack();
+
+        Queue myQueue = new Queue(7);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+
+        System.out.println();
+
+        myQueue.enqueue(45);
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+
+        System.out.println();
+
+        myQueue.dequeue();
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+
+    }
+}
+
